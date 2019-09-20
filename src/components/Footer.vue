586a5d72cacf3ba4e600a3ba93cce6b0912b8ff1
Rukkie555 <terryfrank555@gmail.com>
2019-09-20 22:44:42 +0100

    done with layout of library component

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footer">
+    <v-card raised="10">
+      <v-footer raised v-bind="localAttrs" :padless="padless">
+        <v-card flat tile width="100%" class="white text-center">
+          <v-card-text>
+            <v-btn v-for="icon in icons" :key="icon" class="mx-4 primary--text" icon>
+              <v-icon size="24px">{{ icon }}</v-icon>
+            </v-btn>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text class="primary--text">
+            {{ new Date().getFullYear() }}
+            <strong class="subtitle-1">Skipa</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-email", "mdi-twitter", "mdi-instagram"],
+    items: ["default", "absolute", "fixed"],
+    padless: true,
+    variant: "default"
+  })
+};
+</script>
--- src/components/Library.vue
+++ src/components/Library.vue
@@ -11,18 +11,97 @@
       </v-flex>
     </v-layout>
 
+    <v-layout row wrap>
+      <v-flex class="form">
+        <v-form>
+          <v-container>
+            <v-text-field>
+              <template v-slot:label>Search Skipa</template>
+            </v-text-field>
+          </v-container>
+        </v-form>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
+      <v-flex class="my-5" xs12 sm6 md3 lg3 xl3>
+        <v-hover v-slot:default="{ hover }">
+          <v-card max-width="250" class="mx-auto" :elevation="hover ? 12 : 2">
+            <v-row class="py-4 pl-4">
+              <v-col class="shrink">
+                <v-img height="200" width="200" src="@/assets/add-file.png">
+                  <p>Add a File</p>
+                </v-img>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-hover>
+      </v-flex>
+
+      <v-flex class="my-5" xs12 sm6 md3 lg3 xl3>
+        <v-hover v-slot:default="{ hover }">
+          <v-card max-width="250" class="mx-auto" :elevation="hover ? 12 : 2">
+            <v-row class="py-4 pl-4">
+              <v-col class="shrink">
+                <v-img height="200" width="200" src="@/assets/add-folder.png">
+                  <p>Add a Folder</p>
+                </v-img>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-hover>
+      </v-flex>
+
+      <v-flex class="my-5" xs12 sm6 md3 lg3 xl3>
+        <v-hover v-slot:default="{ hover }">
+          <v-card max-width="250" class="mx-auto" :elevation="hover ? 12 : 2">
+            <v-row class="py-4 pl-4">
+              <v-col class="shrink">
+                <v-img height="200" width="200" src="@/assets/photo.png">
+                  <p>Upload a Photo</p>
+                </v-img>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-flex class="my-5" xs12 sm6 md3 lg3 xl3>
+        <v-hover v-slot:default="{ hover }">
+          <v-card max-width="250" class="mx-auto" :elevation="hover ? 12 : 2">
+            <v-row class="py-4 pl-4">
+              <v-col class="shrink">
+                <v-img height="200" width="200" src="@/assets/video.png">
+                  <p>Upload a Video</p>
+                </v-img>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-hover>
+      </v-flex>
+    </v-layout>
+
     <v-layout>
       <v-flex>
-        
+        <Footer />
       </v-flex>
     </v-layout>
   </div>
 </template>
 
+<script>
+import Footer from "./Footer";
+export default {
+  components: {
+    Footer
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .library {
   margin: 0;
   padding: 0;
+  height: 110vh;
 
   .advert {
     .image {
@@ -36,23 +115,28 @@
         0 80%
       );
 
-      .texts{
+      .texts {
         margin: auto;
         display: block;
         padding: 5%;
         position: relative;
         top: 20%;
-        letter-spacing: .1em;
+        letter-spacing: 0.1em;
 
-        h1{
+        h1 {
           font-weight: lighter;
           font-size: 2.5em;
         }
-        p{
+        p {
           font-size: 1em;
         }
       }
     }
+  }
+
+  .form {
+    max-width: 60%;
+    margin: auto;
   }
 }
 </style>
