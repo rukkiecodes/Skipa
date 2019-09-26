4be9efbfe807b3fb927bd037e0d50ed4aa7c2478
Rukkie555 <terryfrank555@gmail.com>
2019-09-26 04:52:56 +0100

    add firebase

---
+++ src/fb.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+// Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyAb8VSdNflEN4mur7N2Ba8PKsGsPc2Fh6w",
+    authDomain: "skipa-43cff.firebaseapp.com",
+    databaseURL: "https://skipa-43cff.firebaseio.com",
+    projectId: "skipa-43cff",
+    storageBucket: "",
+    messagingSenderId: "299430710385",
+    appId: "1:299430710385:web:c8dadd022456c920d05fd0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
