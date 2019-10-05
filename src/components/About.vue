649cde24e4fd31e332a01fcf21308ed851e92131
Rukkie555 <terryfrank555@gmail.com>
2019-10-05 07:23:00 +0100

    60% done with about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -12,37 +12,98 @@
       </v-row>
     </v-layout>
 
-    <v-layout>
+    <v-layout class="img">
       <v-row justify="center">
         <v-img
-          src="../assets/bg7.jpg"
+          src="../assets/bg8.jpg"
           width="100"
           height="50vh"
           gradient="190deg, rgba(0,0,0,.2), rgba(0,0,0,.9)"
           class="pa-10"
         >
           <v-row align="start" class="fill-height">
-            <p class="white--text headline" style="max-width:20%;">"Skippa is bible of art storage."</p>
+            <p class="p1 white--text">"Skippa is The Bible of Art Storage."</p>
           </v-row>
-          <p class="white--text">Skippa</p>
+          <p class="p2 white--text">Skippa</p>
         </v-img>
       </v-row>
     </v-layout>
+
+    <v-layout class="words my-10">
+      <v-row>
+        <blockquote
+          class="my-5"
+        >Since September 2018, Skippa has been a really helpful tool to both photographers and videographers, in enhancing their popularity and business chances. No bland corporate crap created to fill up a company webpage.</blockquote>
+        <blockquote class="my-5">
+          These days, that’s
+          <a onclick="window.open('https://en.wikipedia.org/wiki/Cloud_storage')">
+            <i>cloud storage</i>
+          </a> followed by a little
+          <a
+            onclick="window.open('https://en.wikipedia.org/wiki/Content_marketing')"
+          >
+            <i>content marketing</i>
+          </a>, and it’s the backbone of getting your company noticed, attracting prospects, and turning them into loyal customers and clients.
+        </blockquote>
+        <blockquote
+          class="my-5"
+        >Our Mission is to help you create the kind of audience-focused content that helps you reach your business goals.</blockquote>
+
+        <h1>Your free content marketing education:</h1>
+
+        <blockquote
+          class="my-5"
+        >How did we know back in 2006 that content marketing would become so important? Like, a $44 billion a year industry important?</blockquote>
+        <blockquote
+          class="my-5"
+        >Well, because content has been a smart way to find more customers and clients for at least 100 years. But now the internet has empowered prospective customers and clients to the point that content has become the smartest way to market online.</blockquote>
+        <blockquote
+          class="my-5"
+        >Today, Skippa brings you original, relevant content every week about what’s working right now in online content marketing. And we’re not too humble to say it’s the most popular content marketing and writing blog on the planet.</blockquote>
+
+        <i>"Skippa is The Bible of Art Storage."</i>
+      </v-row>
+    </v-layout>
+
+    <v-layout class="words">
+      <v-flex xs12 sm12 md12 lg12 xl12>
+        <p>What can you expect?</p>
+      </v-flex>
+    </v-layout>
+
+    <v-layout class="words">
+      <v-row>
+        <ul>
+          <li>It’s not (just) about Storage.</li>
+          <li>It’s not (just) about Safty.</li>
+          <li>It’s not (just) about social media.</li>
+          <li>It’s not (just) about blogging or email marketing or conversion.</li>
+        </ul>
+
+        <blockquote class="my-10">
+            Today, it’s always a bigger picture. You need to understand all the facets of effective storage and  content marketing, and we cover the gamut, daily … for free.
+        </blockquote>
+      </v-row>
+    </v-layout>
+
+    <Footer />
   </v-container>
 </template>
 
 <script>
 import Nav1 from "./Nav1";
+import Footer from "./Footer";
 export default {
   components: {
-    Nav1
+    Nav1,
+    Footer
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .caption {
-  width: 70%;
+  width: 75%;
   text-align: center;
   margin: auto;
   @media (max-width: 600px) {
@@ -57,6 +118,43 @@ export default {
     @media (max-width: 768px) {
       font-size: 2em;
     }
+  }
+}
+.img {
+  width: 75%;
+  margin: auto;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  .p1 {
+    font-size: 1.9em;
+    font-weight: 300;
+    width: 35%;
+    @media (max-width: 768px) {
+      font-size: 1.5em;
+      width: 80%;
+    }
+  }
+
+  .p2 {
+    font-size: 1.2em;
+    letter-spacing: 0.1em;
+  }
+}
+.words {
+  width: 75%;
+  margin: auto;
+  @media (max-width: 768px) {
+    width: 98%;
+  }
+
+  blockquote,
+  i,
+  p,
+  ul li {
+    font-size: 1.3em;
+    color: rgba(0, 0, 0, 0.8);
   }
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footer">
+    <v-card raised>
+      <v-footer raised :padless="padless">
+        <v-card flat tile width="100%" class="white text-center">
+          <v-card-text>
+            <v-btn v-for="icon in icons" :key="icon" class="mx-4 black--text" icon>
+              <v-icon size="24px">{{ icon }}</v-icon>
+            </v-btn>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text class="black--text">
+            {{ new Date().getFullYear() }}
+            <strong class="subtitle-1">Skipa</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-email", "mdi-twitter", "mdi-instagram"],
+    items: ["default", "absolute", "fixed"],
+    padless: true,
+    variant: "default"
+  })
+};
+</script>
