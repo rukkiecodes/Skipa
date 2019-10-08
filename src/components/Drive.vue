6f14dceee40d1edcc1395d18a6bebbeef76af05a
Rukkie555 <terryfrank555@gmail.com>
2019-10-08 01:34:55 +0100

    working on upload dialogs

--- src/components/Drive.vue
+++ src/components/Drive.vue
@@ -30,6 +30,168 @@
         </v-hover>
       </v-row>
     </v-layout>
+
+    <v-layout row wrap>
+      <v-row justify="center">
+        <v-col align-self="center">
+          <v-card flat class="overflow-hidden upload-card">
+            <v-card-title>
+              <v-card-text>Upload Options</v-card-text>
+            </v-card-title>
+            <v-app-bar dense color="white" dark>
+              <v-row justify="space-around">
+
+                  <!-- dialog for random file upload -->
+                <v-dialog v-model="dialog" persistent max-width="600px">
+                  <template v-slot:activator="{ on }">
+                    <v-btn dark v-on="on" tile color="white" text>
+                      <v-icon color="grey darken-4">mdi-file</v-icon>
+                      <p class="my-2 black--text">File</p>
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">Upload file</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-file-input
+                              multiple
+                              label="File input"
+                              accept="image/*|audio/*|video/*"
+                            ></v-file-input>
+                          </v-col>
+                          <v-col cols="12">
+                            <!-- <v-text-field label="Name*" required></v-text-field> -->
+                            <v-text-field label="Name / Company Name*" required>
+                              <v-icon slot="prepend">mdi-account</v-icon>
+                            </v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-textarea
+                              name="input-7-1"
+                              label="Preview"
+                              placeholder="Write a short preview"
+                              hint="How would u like people to think about you work"
+                            ></v-textarea>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                      <div class="flex-grow-1"></div>
+                      <v-btn class="red--text" text @click="dialog = false">
+                        <v-icon>mdi-cancel</v-icon>Cancel
+                      </v-btn>
+                      <v-btn class="green--text" text @click="dialog = false">
+                        <v-icon>mdi-cloud-upload</v-icon>Save
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+
+                <!-- dialog for only image upload -->
+                <v-dialog v-model="dialog2" persistent max-width="600px">
+                  <template v-slot:activator="{ on }">
+                    <v-btn v-on="on" tile color="white" class="black--text" text>
+                      <v-icon color="grey darken-4">mdi-image</v-icon>
+                      <p class="my-2 black--text">Image</p>
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">Upload an Image</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-file-input multiple label="File input" accept="image/*"></v-file-input>
+                          </v-col>
+                          <v-col cols="12">
+                            <!-- <v-text-field label="Name*" required></v-text-field> -->
+                            <v-text-field label="Name / Company Name*" required>
+                              <v-icon slot="prepend">mdi-account</v-icon>
+                            </v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-textarea
+                              name="input-7-1"
+                              label="Preview"
+                              placeholder="Write a short preview"
+                              hint="How would u like people to think about you work"
+                            ></v-textarea>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                      <div class="flex-grow-1"></div>
+                      <v-btn class="red--text" text @click="dialog2 = false">
+                        <v-icon>mdi-cancel</v-icon>Cancel
+                      </v-btn>
+                      <v-btn class="green--text" text @click="dialog2 = false">
+                        <v-icon>mdi-cloud-upload</v-icon>Save
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+
+                <!-- dialod for only video upload -->
+                <v-dialog v-model="dialog3" persistent max-width="600px">
+                  <template v-slot:activator="{ on }">
+                    <v-btn v-on="on" tile color="white" class="black--text" text>
+                      <v-icon color="grey darken-4">mdi-video</v-icon>
+                      <p class="my-2 black--text">Video</p>
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">Upload Video</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-file-input multiple label="File input" accept='video/*'></v-file-input>
+                          </v-col>
+                          <v-col cols="12">
+                            <!-- <v-text-field label="Name*" required></v-text-field> -->
+                            <v-text-field label="Name / Company Name*" required>
+                              <v-icon slot="prepend">mdi-account</v-icon>
+                            </v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-textarea
+                              name="input-7-1"
+                              label="Preview"
+                              placeholder="Write a short preview"
+                              hint="How would u like people to think about you work"
+                            ></v-textarea>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                      <div class="flex-grow-1"></div>
+                      <v-btn class="red--text" text @click="dialog3 = false">
+                        <v-icon>mdi-cancel</v-icon>Cancel
+                      </v-btn>
+                      <v-btn class="green--text" text @click="dialog3 = false">
+                        <v-icon>mdi-cloud-upload</v-icon>Save
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+            </v-app-bar>
+            <v-divider></v-divider>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-layout>
   </v-container>
 </template>
 
@@ -37,7 +199,11 @@
 export default {
   data: () => ({
     labels: ["12am", "3am", "6am", "9am", "12pm", "3pm", "6pm", "9pm"],
-    value: [200, 675, 410, 390, 310, 460, 250, 240]
+    value: [200, 675, 410, 390, 310, 460, 250, 240],
+    collapseOnScroll: true,
+    dialog: false,
+    dialog2: false,
+    dialog3: false
   })
 };
 </script>
@@ -47,11 +213,15 @@ export default {
   top: -24px;
   position: relative;
 }
-.statistics-card {
+.statistics-card,
+.upload-card {
   width: 70%;
   margin: auto;
   @media (max-width: 768px) {
     width: 100%;
   }
+}
+.toolbar-title {
+  color: rgba(0, 0, 0, 0.8);
 }
 </style>
