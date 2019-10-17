8f405de7ae47ed5629f6111d6e1e33493183ecac
Rukkie555 <terryfrank555@gmail.com>
2019-10-17 22:27:46 +0100

    made changes to home page and about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,18 +1,35 @@
 <template>
   <v-container class="about">
-    <v-layout>
-      <v-flex>
-        <Nav1 />
-      </v-flex>
-    </v-layout>
-    <AboutContent/>
+    <nav>
+      <v-toolbar flat color="transparent">
+        <v-toolbar-title style="display:flex;">
+          <router-link to="/">
+            <v-img width="2em" src="@/assets/logo.png"></v-img>
+          </router-link>
+          <p class="my-2 mx-2 white--text hidden-sm-and-down">Skippa</p>
+        </v-toolbar-title>
+
+        <div class="flex-grow-1"></div>
+
+        <v-btn-toggle tile color="deep-purple accent-3" group>
+          <v-btn value="right">
+            <router-link class="grey--text text--darken-4" style="text-decoration:none;" to="/about">About</router-link>
+          </v-btn>
+
+          <v-btn class="rout-color" value="justify">
+            <router-link class="grey--text text--darken-4" style="text-decoration:none;" to="/">Home</router-link>
+          </v-btn>
+        </v-btn-toggle>
+      </v-toolbar>
+    </nav>
+    <AboutContent />
     <Footer class="footer" />
   </v-container>
 </template>
 
 <script>
 import Nav1 from "./Nav1";
-import AboutContent from "./AboutContent"
+import AboutContent from "./AboutContent";
 import Footer from "./Footer";
 export default {
   components: {
@@ -26,8 +43,8 @@ export default {
 <style lang="scss" scoped>
 .about {
   background: #fff;
-  
-  .footer{
+
+  .footer {
     width: 75%;
     margin: auto;
     @media (max-width: 768px) {
