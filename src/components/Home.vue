3f7dc79d8367bee042cb4c0297b8874a8fd11896
Rukkie555 <terryfrank555@gmail.com>
2019-10-17 04:30:24 +0100

    moved home page cards to vuex store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,39 +27,39 @@
 
     <v-layout class="imgk mx-auto" row wrap>
       <v-layout row wrap class="imgs my-10">
-        <v-flex v-for="(card, id) in cards" :key="id" class="my-5" xs12 sm6 md4 lg4 xl4>
+        <v-flex v-for="(homeCard, id) in homeCards" :key="id" class="my-5" xs12 sm6 md4 lg4 xl4>
           <v-card class="v-card mx-auto" raised>
             <v-card-title class="pa-2 card-title">
               <v-layout>
                 <v-flex xs3 sm3 md3 lg3 xl3>
                   <v-avatar class="card-avatar">
-                    <v-img :src="card.avatar" width="1.2em" height="auto"></v-img>
+                    <v-img :src="homeCard.avatar" width="1.2em" height="auto"></v-img>
                   </v-avatar>
                 </v-flex>
 
                 <v-flex xs7 sm7 md7 lg7 xl7>
                   <div class="title-texts mt-n1">
-                    <p>{{ card.title }}</p>
-                    <h6 class="mt-n8">{{ card.review }}</h6>
+                    <p>{{ homeCard.title }}</p>
+                    <h6 class="mt-n8">{{ homeCard.review }}</h6>
                   </div>
                 </v-flex>
 
                 <v-flex xs3 sm3 md3 lg3 xl3>
                   <v-btn text icon color="grey darken-3">
-                <v-icon>{{ card.buttons.download }}</v-icon>
+                <v-icon>{{ homeCard.buttons.download }}</v-icon>
               </v-btn>
                 </v-flex>
               </v-layout>
             </v-card-title>
-            <v-img :src="card.img" height="auto"></v-img>
+            <v-img :src="homeCard.img" height="auto"></v-img>
 
             <v-card-actions>
               <v-row justify="space-around">
                 <v-btn text icon color="red">
-                  <v-icon>{{ card.buttons.heart }}</v-icon>
+                  <v-icon>{{ homeCard.buttons.heart }}</v-icon>
                 </v-btn>
                 <v-btn text icon color="grey darken-3">
-                  <v-icon>{{ card.buttons.eye }}</v-icon>
+                  <v-icon>{{ homeCard.buttons.eye }}</v-icon>
                 </v-btn>
               </v-row>
             </v-card-actions>
@@ -75,58 +75,19 @@
 <script>
 import Nav1 from "./Nav1";
 import Footer from "./Footer";
+import { mapState } from "vuex"
 export default {
   data: () => ({
-    cards: [
-      {
-        img: require("../assets/bg1.jpg"),
-        title: "Mary Tyler",
-        review: "Runing out of time",
-        avatar: require("../assets/av3.png"),
-        buttons: {
-          heart: "mdi-heart",
-          eye: "mdi-eye",
-          download: "mdi-download"
-        }
-      },
-      {
-        img: require("../assets/bg2.jpg"),
-        title: "John Doe",
-        review: "Capture your favrite moments",
-        avatar: require("../assets/av2.jpg"),
-        buttons: {
-          heart: "mdi-heart",
-          eye: "mdi-eye",
-          download: "mdi-download"
-        }
-      },
-      {
-        img: require("../assets/bg3.jpg"),
-        title: "Jeorge SP",
-        review: "Where do you like it",
-        avatar: require("../assets/av1.jpg"),
-        buttons: {
-          heart: "mdi-heart",
-          eye: "mdi-eye",
-          download: "mdi-download"
-        }
-      },
-      {
-        img: require("../assets/bg4.jpg"),
-        title: "Tosin Leni",
-        review: "Bad ass Videographer",
-        avatar: require("../assets/av4.png"),
-        buttons: {
-          heart: "mdi-heart",
-          eye: "mdi-eye",
-          download: "mdi-download"
-        }
-      }
-    ]
+    
   }),
   components: {
     Nav1,
     Footer
+  },
+  computed:{
+    ...mapState([
+      'homeCards'
+    ])
   }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,54 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    homeCards: [
+      {
+        img: require("./assets/bg1.jpg"),
+        title: "Mary Tyler",
+        review: "Runing out of time",
+        avatar: require("./assets/av3.png"),
+        buttons: {
+          heart: "mdi-heart",
+          eye: "mdi-eye",
+          download: "mdi-download"
+        }
+      },
+      {
+        img: require("./assets/bg2.jpg"),
+        title: "John Doe",
+        review: "Capture your favrite moments",
+        avatar: require("./assets/av2.jpg"),
+        buttons: {
+          heart: "mdi-heart",
+          eye: "mdi-eye",
+          download: "mdi-download"
+        }
+      },
+      {
+        img: require("./assets/bg3.jpg"),
+        title: "Jeorge SP",
+        review: "Where do you like it",
+        avatar: require("./assets/av1.jpg"),
+        buttons: {
+          heart: "mdi-heart",
+          eye: "mdi-eye",
+          download: "mdi-download"
+        }
+      },
+      {
+        img: require("./assets/bg4.jpg"),
+        title: "Tosin Leni",
+        review: "Bad ass Videographer",
+        avatar: require("./assets/av4.png"),
+        buttons: {
+          heart: "mdi-heart",
+          eye: "mdi-eye",
+          download: "mdi-download"
+        }
+      }
+    ]
+  },
   mutations: {},
   actions: {}
 });
